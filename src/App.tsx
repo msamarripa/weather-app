75a98699a5f57820bdc6e142debff20f359daeb5
Max Samarripa <max.samarripa@gmail.com>
2022-03-23 19:48:30 -0600

    change project structure

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,7 @@ import {
   Grid,
   theme,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Logo } from "./Logo"
+import { ColorModeSwitcher } from "./components/ColorModeSwitcher"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
@@ -18,7 +17,6 @@ export const App = () => (
       <Grid minH="100vh" p={3}>
         <ColorModeSwitcher justifySelf="flex-end" />
         <VStack spacing={8}>
-          <Logo h="40vmin" pointerEvents="none" />
           <Text>
             Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
           </Text>
