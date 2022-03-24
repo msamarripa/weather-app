d12bc22f42b63ccc4690299fd8c4f1ad2bd0b96b
Max Samarripa <max.samarripa@gmail.com>
2022-03-23 20:00:50 -0600

    redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,17 @@ import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import ReactDOM from "react-dom"
 import { App } from "./App"
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
 import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
 
 ReactDOM.render(
   <React.StrictMode>
     <ColorModeScript />
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root"),
 )
